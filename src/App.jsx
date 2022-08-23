87f9b79beb208cd9e72e0da09ea402a49232976c
siraj <sirajbhutta50@gmail.com>
2022-08-23 13:51:10 +0500

    project setup and file structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Components/Header"
+import Projects from "./Components/Projects"
+import Footer from "./Components/Footer"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Projects />
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Styled/Utilities.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    font-family: 'Open Sans';
+    font-size: 48px;
+    font-weight: 700;
+    margin-bottom: 30px;
+`
+export const Heading = styled.h2`
+    font-family: 'Open Sans';
+    font-size: 36px;
+    font-weight: 700;
+    margin-bottom: 18px;
+`
+export const Text = styled.p`
+    font-family: 'Open Sans';
+    font-size: 20px;
+    font-weight: 700;
+    margin-bottom: 16px;
+`
+export const ThinText = styled.p`
+    font-family: 'Open Sans';
+    font-size: 24px;
+    font-weight: 300;
+`
+export const LinkText = styled.p`
+    font-family: 'Open Sans';
+    font-size: 24px;
+    font-weight: 300;
+`
