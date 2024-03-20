aeceed9c9082ab0f2c53ea850e5ce9e9c4df6785
engsiraj <sirajbhutta50@gmail.com>
2024-03-20 03:11:13 -0700

    📚 add hover state

--- src/library/Button.jsx
+++ src/library/Button.jsx
@@ -3,19 +3,24 @@ import { color, radius } from "../assets";
 
 const blueBtn = `
    border: 0.125rem solid ${color.blue};
-   background-color: ${color.bluebg}
+   background-color: ${color.bluebg};
+   :hover {background-color: ${color.blue};}
 `;
 const greenBtn = `
    border: 0.125rem solid ${color.green};
-   background-color: ${color.greenbg}
+   background-color: ${color.greenbg};
+   :hover {background-color: ${color.green};}
+   
 `;
 const redBtn = `
    border: 0.125rem solid ${color.red};
-   background-color: ${color.redbg}
+   background-color: ${color.redbg};
+   :hover {background-color: ${color.red};}
 `;
 const yellowBtn = `
    border: 0.125rem solid ${color.yellow};
-   background-color: ${color.yellowbg}
+   background-color: ${color.yellowbg};
+   :hover {background-color: ${color.yellow};}
 `;
 
 export const Button = styled.button`
@@ -29,7 +34,7 @@ export const Button = styled.button`
   ${(p) => {
     if (p.blue) return `${blueBtn}`;
     else if (p.green) return `${greenBtn}`;
-    else if (p.yellow) return `${yellowBtn}`;
+    else if (p.yellow) return `${yellowBtn} `;
     else if (p.red) return `${redBtn}`;
   }}
 `;
