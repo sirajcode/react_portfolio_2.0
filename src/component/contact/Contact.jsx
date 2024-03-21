8873a6480a43ec1fe2343dff3dc3a5b25c44398f
engsiraj <sirajbhutta50@gmail.com>
2024-03-20 21:30:04 -0700

    📞 add contact

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -1,9 +1,21 @@
-
-
-const contact = () => {
+import { ContactSection } from "./Contact.styled";
+import { Text, HrefLink } from "../../library";
+import { ContactIcon } from "../../assets";
+export const Contact = () => {
   return (
-    <div>contact</div>
-  )
-}
-
-export default contact
+    <ContactSection>
+      <Text margin_top="30px" margin_bottom="30px" font="title">
+        <ContactIcon /> Cotact
+      </Text>
+      <Text font="text" margin_bottom="20px">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eaque,
+        vel praesentium ea itaque tenetur nostrum,
+      </Text>
+      <Text font="text">Lorem ipsum dolo:</Text>
+      <HrefLink textColor="green">sirjbhutta50@gmail.com</HrefLink>
+      <Text margin_bottom="20px"></Text>
+      <Text font="text">Lorem ipsum dolo:</Text>
+      <HrefLink textColor="green">sirjbhutta50@gmail.com</HrefLink>
+    </ContactSection>
+  );
+};
---
+++ src/component/contact/Contact.styled.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const ContactSection = styled.div`
+  margin-top: 50px;
+  margin-bottom: 50px;
+`;
