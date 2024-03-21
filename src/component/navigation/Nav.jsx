8c83eaa46c085ac5ba3fc7b755b92687a7b41064
engsiraj <sirajbhutta50@gmail.com>
2024-03-21 01:45:36 -0700

    📌 navigation

--- src/component/navigation/Nav.jsx
+++ src/component/navigation/Nav.jsx
@@ -1,9 +1,17 @@
-
-
-const Nav = () => {
+import { Navigation } from "./Nav.styled";
+import { Button } from "../../library";
+export const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
-
-export default Nav
+    <>
+      <Navigation>
+        <div>
+          <Button margin_right='10px' green>projects</Button>
+          <Button red>About me </Button>
+        </div>
+        <div>
+          <Button yellow>download resume</Button>
+        </div>
+      </Navigation>
+    </>
+  );
+};
---
+++ src/component/navigation/Nav.styled.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Navigation = styled.div`
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
