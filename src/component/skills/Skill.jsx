94a76f33e60e2b9d9b29bfbb54345a971382a998
engsiraj <sirajbhutta50@gmail.com>
2024-03-21 01:48:39 -0700

    ✨ tech stack

--- src/component/skills/Skill.jsx
+++ src/component/skills/Skill.jsx
@@ -1,7 +1,27 @@
-const Skill = () => {
-  return (
-    <div>Skill</div>
-  )
-}
+import { Text } from "../../library";
+import { StackName } from "./Skill.styled";
+import { SkillsSection } from "./Skill.styled";
 
-export default Skill
+export const Skill = () => {
+  return (
+    <SkillsSection>
+      <Text font="mdtitle">
+        skills
+      </Text>
+      <StackName>Frontend: </StackName>
+      <StackName>html </StackName>
+      <StackName>css </StackName>
+      <StackName>javascript </StackName>
+      <StackName>typescript </StackName>
+      <StackName>React js </StackName>
+      <StackName>Next js </StackName>
+      <Text></Text>
+      <StackName>backend: </StackName>
+      <StackName>python </StackName>
+      <StackName>django </StackName>
+      <StackName>django rest-framework </StackName>
+      <StackName>flask </StackName>
+      <StackName>rest api </StackName>
+    </SkillsSection>
+  );
+};
---
+++ src/component/skills/Skill.styled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StackName = styled.span`
+  font-size: 18px;
+  padding-right: 7px;
+`;
+export const SkillsSection = styled.div`
+  margin-bottom: 50px;
+`;
