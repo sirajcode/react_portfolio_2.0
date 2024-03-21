51a8fe9141a26dd8087d5284bf31d9a0d4474c60
engsiraj <sirajbhutta50@gmail.com>
2024-03-20 21:28:23 -0700

    🤵 add about section

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,9 +1,24 @@
+import { Text } from "../../library";
+import { AboutIcon } from "../../assets";
+import { AboutSection } from "./About.styled";
 
-
-const About = () => {
+export const About = () => {
   return (
-    <div>About</div>
-  )
-}
-
-export default About
+    <>
+      {" "}
+      <AboutSection>
+        <Text font="title">
+          <AboutIcon /> About me
+        </Text>
+        <Text margin_top="20px" font="text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
+          eius voluptatem. Quibusdam magni vel quam veniam. Delectus nemo cumque
+        </Text>
+        <Text margin_top="20px" font="text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
+          eius voluptatem. Quibusdam magni vel quam veniam. Delectus nemo cumque
+        </Text>
+      </AboutSection>
+    </>
+  );
+};
---
+++ src/component/about/About.styled.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+
+export const AboutSection = styled.div`
+  margin-top: 50px;
+  margin-bottom: 30px;
+`;
