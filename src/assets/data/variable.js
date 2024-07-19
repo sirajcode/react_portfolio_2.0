103a7431184ad05c35dcdf8e475dea74b6d52c97
engsiraj <sirajbhutta50@gmail.com>
2024-07-19 16:49:55 +0500

    update(ui): data and icons

--- src/assets/data/variable.js
+++ src/assets/data/variable.js
@@ -1,26 +1,17 @@
 export const color = {
-    white: 'white',
-    green: '#19ce73',
-    //border color
-    border: 'rgba(255, 255, 255, 0.2)',
-    // font colors and backgrounds
-    Dark50: "#E0E0E0",
-    Dark100: "#BDBDBD",
-    Dark200: "#616161",
-    Dark300: "#212121",
-    Dark400: "#1b1b1b",
-    Dark500: "#242424",
-    //button colors
+    light: "#dadada",
+    dark: "#333333"
 }
-export const radius = {
-    round: '1.125rem',
+export const radius = { 
+    round: '10px',
 }
 
 //text sizes 
 export const textSize = {
+    lg: '40px',
     title: '32px',
     mdtitle: '28px',
     smtitle: '20px',
-    text: '17px',
-    button: '15px',
+    text: '16px',
+    button: '14px',
 }
