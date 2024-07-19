30bf57e44a636177395150503982b1c1fe7324ca
engsiraj <sirajbhutta50@gmail.com>
2024-07-19 16:55:30 +0500

    update(ui): change ui

--- src/pages/home/Home.styled.jsx
+++ src/pages/home/Home.styled.jsx
@@ -4,8 +4,8 @@ export const HeadContainer = styled.div`
   margin: 3% 3% 0% 3%;
 `;
 export const Container = styled.div`
-  padding-left: 20%;
-  padding-right: 20%;
+  padding-left: 15%;
+  padding-right: 15%;
   @media (max-width: 767px) {
     padding-left: 10%;
     padding-right: 10%;
