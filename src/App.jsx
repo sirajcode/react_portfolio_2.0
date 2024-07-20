269de51b63de2e378dd914da24571dfa5d355625
engsiraj <sirajbhutta50@gmail.com>
2024-07-20 23:36:14 +0500

    fix(links): broken links

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import { HomePage, ErrorPage } from "./pages";
+import { HomePage, ErrorPage, Project } from "./pages";
 
 // import Page from "./Components/Page/Page";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<HomePage />} />
+      <Route path="/project" element={<Project />} />
       <Route path="*" element={<ErrorPage />} />
     </Route>
   )
