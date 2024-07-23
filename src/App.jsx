c0a7bc242599c7662ff0087fdfbf13c891f7b9d0
engsiraj <sirajbhutta50@gmail.com>
2024-07-23 15:18:25 +0500

    add(page): add dynamic route

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,25 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+
 import { HomePage, ErrorPage, Project } from "./pages";
 
-// import Page from "./Components/Page/Page";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<HomePage />} />
-      <Route path="/project" element={<Project />} />
+      <Route path="/project/:id" element={<Project />} />
       <Route path="*" element={<ErrorPage />} />
     </Route>
   )
 );
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
